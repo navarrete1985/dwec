aae977497ea3bf8456b471ebf421c79c4ea460f9
Nacho <nacho.pena.navarrete@gmail.com>
2018-09-24 12:46:54 +0200

    clase 24-9

---
+++ Tema2/funciones_variables/script.js
@@ -0,0 +1,57 @@
+var numero = 5;
+console.log(numero);
+sumar(1, 2);
+//console.log(resultado); //El resultado es undefined puesto que no tenemos alcance a esta variable
+console.log(numero);
+
+contador();
+//console.log(resultado2); //la variable resultado2 nos es visible puesto que está declarada en una función, y no tenemos visibilidad
+
+function contador(){
+    let resultado = 0;
+    var resultado2 = 0;
+    for (let i=0; i<20; i++){
+        resultado += 1;
+        resultado2 += 1;
+    }
+
+    //Con esto lo que probamos es el tipo de dato
+    if ( typeof resultado == "number"){
+        console.log("El resultado es tipo numérico")
+    }else{
+        console.log("El resultado es otra cosa que no es número")
+    }
+
+    console.log("resultado --> ", resultado, "\nresultado2 -->", resultado2);
+    
+    //La variable i no es visible fuera del for puesto que está declarada como let
+}
+
+function sumar(numero1, numero2){
+    var resultado = 0;
+    resultado = numero1 + numero2;
+    console.log(resultado);
+    numero = resultado;
+}
+
+var usuario = (function(){
+    var nombre = "un nombre";
+    var apellidos = "apellidos"
+
+    var mostrar = function(){
+        return console.log(nombre + apellidos);
+    }
+
+    var setNombre = function(nombre){
+        this.nombre = nombre;
+        return true;
+    }
+
+    var setApellidos = function(apellido){
+        this.apellidos = apellido;
+        return true;
+    }
+})();
+
+
+console.log("El nombre del objeto es --> ", usuario.mostrar())
