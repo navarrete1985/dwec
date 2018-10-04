55ec6ab2022e41d4ee80f6dc66ff01b6cc95e1c8
Nacho <nacho.pena.navarrete@gmail.com>
2018-10-04 14:33:35 +0200

    04-10-18

--- Tema2/ejercicios/palindromo/script.js
+++ Tema2/ejercicios/palindromo/script.js
@@ -1,22 +1,19 @@
 (function (){
     document.getElementById('comprobar').addEventListener('click', (event) =>{
-        let entrada = document.getElementById('palabra').value.trim().replace(/\s/g, '');
-        if(entrada.length <= 1){
-            alert('Introduzca una cadena de más de 1 caracter');
-            return;
-        }
-        let entradaReverse = entrada.split('').reverse();
-        entrada = entrada.split('');
-        let esPalindromo = true;
-        for(let i=0; i<entrada.length && palindromo; i++){
-            if(entrada[i] != entradaReverse[i]){
-                esPalindromo = false;
-            }
-        }
-        if(esPalindromo){
-            alert('La palabra introducida es un palíndromo');
-        }else{
-            alert('La palabra introducida no es un palíndromo');
-        }
+        alert(palindromo(document.getElementById('palabra').value.trim().replace(/\s/g, '')));
     })
+
+    function palindromo(cadena){
+        let result = 'La cadena introducida no es nun palíndromo';
+        let entradaReverse = cadena.split('').reverse().join('');
+
+        if(cadena.length <= 1){
+            result = 'Introduzca una cadena de más de 1 caracter';
+        }else if (cadena == entrada){
+            result = 'La cadena introducida es un palíndromo';
+        }
+
+        return result
+    }
+
 })();
