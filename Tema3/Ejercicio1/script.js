63e78a85150e158fc910a566a56088f89807f9af
Luis Ignacio <nacho_pena@hotmail.com>
2018-10-10 18:30:08 +0200

    Ejercicio1 tema3

--- Tema3/Ejercicio1/script.js
+++ Tema3/Ejercicio1/script.js
@@ -2,17 +2,33 @@
 
     let lista = document.getElementById("listaCanciones");
     let input = document.getElementById("cancion");
+    let canciones = lista.children;
     let btn = document.getElementById("botonadd").addEventListener('click', function(){
-        if (input.value.trim() != ""){
+        let entrada = input.value.trim();
+        if (entrada === ""){
+            alert("Inserte una canción para poder añadirla.");
+        }else if (isRepeat(canciones, entrada)){
+            alert("La canción que intenta añadir está ya en la lista!");
+        }else{
             addCancion(input.value, lista);
         }
+        input.value = "";
+        input.focus();
     });
     
-
     function addCancion(nombreCancion, nodo){
         let elemento = document.createElement("li");
         elemento.textContent = nombreCancion;
         nodo.appendChild(elemento);
+    }
+
+    function isRepeat(lista, valor){
+        for (let item of lista){
+            if (item.textContent.toLowerCase() === valor.toLowerCase()){
+                return true;
+            }
+        }
+        return false;
     }
 
 })();
