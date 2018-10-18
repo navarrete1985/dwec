85dddd77e22de1275a0d6306ec8d3edbeaee52b3
Nacho <nacho.pena.navarrete@gmail.com>
2018-10-18 14:40:37 +0200

    Clase 18-10-18

---
+++ Tema3/Ejercicio3/script.js
@@ -0,0 +1,34 @@
+(function(){
+
+    let imagenes = {
+        imagenes: new Array(
+            'cala_menuda.jpg',
+            'playa_meron.jpg',
+            'playa_teresitas.jpg',
+            'san_futadera.jpg',
+            'valdevaqueros.jpg',
+            'cavalleria.jpg',
+            'taganana.jpg'
+        ),
+        descripcion: new Array(
+            'Cala Menuda',
+            'Playa Merón',
+            'Playa de las Teresitas',
+            'Playa de San Furtadera',
+            'Playa de Valdevaqueros',
+            'Plata de Cavallería',
+            'Plata de Taganana'
+        )
+    };
+
+    let slider = (function)
+
+})();
+
+
+/*
+Realiza una página que saque una serie de diapositivas a partir de unos ficheros
+con fotos. El pase de diapositivas será automático cada 2 segundos y aparecerá
+además de la foto el título de la misma.
+Puedes  añadir  las  funcionalidades  que  se  te  ocurran.
+*/
