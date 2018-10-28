e0b0d9c8a0463ad20bd1ada16a9294cc30679fda
Luis Ignacio <nacho_pena@hotmail.com>
2018-10-29 00:28:37 +0100

    ejercicio formulario

--- Tema5/ejercicioFormulario1/script.js
+++ Tema5/ejercicioFormulario1/script.js
@@ -20,4 +20,156 @@ opción no se verifique no se habilitará el botón de envío.
 
 (function() {
 
+    let form = document.getElementById('form');
+    let number1 = generateNumber(0, 100);
+    let number2 = generateNumber(0, 100);
+    let label = document.getElementById('label');
+    label.textContent += ' ' + number1 + ' + ' + number2;
+    form.addEventListener('submit', checkForm);
+    form.addEventListener('reset', checkReset);
+    document.getElementById('condiciones').addEventListener('change', function(event){
+        let btnSubmit = form.querySelector('input[type="submit"]');
+        btnSubmit.disabled = (event.target.checked ? false : true);
+    });
+    window.addEventListener('beforeunload', function(event){
+        event.returnValue = 'Seguro de que quieres abandonar la página?';
+    });
+
+    function checkForm(event) {
+        event.preventDefault();
+        let nombre = document.getElementById('nombre');
+        let passwd = document.getElementById('pswd');
+        let adge = document.getElementById('nacimiento');
+        let inputOp = document.getElementById('verify');
+        let isValidName = checkName(nombre, 3, 15);
+        let isValidPass = checkPassword(passwd);
+        let isValidEmail = checkMail(document.getElementById('email'));
+        if (isValidPass){
+            isValidPass = checkRepeatPass(passwd, document.getElementById('rpswd'));
+        }
+        let isValidGenre = checkBoxCheck(document.getElementsByName('genero'));
+        if (!isValidGenre) {
+            addErrorToElement(document.querySelector('div.sexo'), 'Campo obligatorio, marque una opción');
+        }else {
+            removeErrorToElement(document.querySelector('div.sexo'));
+        }
+
+        let isValidage = checkAge(document.getElementById('nacimiento').value, 18);
+        isValidage ? removeErrorToElement(nacimiento.previousElementSibling) : addErrorToElement(nacimiento.previousElementSibling, 'Campo obligatorio y mayor de 18');
+
+        let isValidOp = (inputOp.value == number1+number2 ? true : false);
+        isValidOp ? removeErrorToElement(inputOp) : addErrorToElement(inputOp, 'Operación matemática de verificación erronea');
+
+    }
+
+    function checkReset(event) {
+        if (!confirm('Seguro que quiere resetear el formulario')){
+            event.preventDefault();
+        }else {
+            form.querySelector('input[type="submit"]').disabled = true;
+        }   
+    }
+
+    function checkName(node, minSize, maxSize) {
+        let result = true;
+        /*/^([a-zA-Z0-9\s]{" + minSize + "," + maxSize + "})$/*/ 
+        let regex = new RegExp("^([a-zA-Z0-9\\s]{" + minSize + "," + maxSize + "})$");
+        let cadena = node.value.trim();
+        if (!regex.test(cadena)){
+            addErrorToElement(node, 'Campo inválido, cadena alfanumerica entre ' + minSize + ' y ' + maxSize + ' carateres');
+            result = false;
+        }else {
+            removeErrorToElement(node);
+        }
+        return result;
+    }
+
+    function checkMail(node) {
+        let result = true;
+        let regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (!regex.test(node.value)) {
+            addErrorToElement(node, "Formato de email inválido!");
+            result = false;
+        }else {
+            removeErrorToElement(node);
+        }
+        return ;
+    }
+
+    function checkPassword(node) {
+        let result = true;
+        var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)([A-Za-z\d]|[^ ]){6,15}$/;
+        if(!regex.test(node.value)) {
+            addErrorToElement(node, 'Contraseña inválida, debe estar formada por numeros, subrayado y letras, al menos 1 mayuscula, 1 minuscula y un número');
+            result = false;
+        }else {
+            removeErrorToElement(node);
+        }
+
+        return result;
+    }
+
+    function checkRepeatPass(pass, rpass) {
+        result = true;
+        if (pass.value !== rpass.value) {
+            addErrorToElement(rpass, 'Las contraseñas no coinciden!');
+            result = false;
+        }else{
+            removeErrorToElement(rpass);
+        }
+    }
+
+    function checkBoxCheck(nodes) {
+        let result = false;
+        for (let item of nodes) {
+            if (item.checked){
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    function checkAge(date, years) {
+        if (date !== null && getAge(date) >= years) {
+            return true;
+        }
+        return false;
+    }
+
+    function addErrorToElement(node, erorr) {
+        previousNode = node.previousSibling;
+        let find = false;
+        if (previousNode !== undefined && previousNode.tagName === 'DIV') {
+            find = true;
+        }
+        if (!find) {
+            let error = document.createElement('div');
+            error.textContent = erorr;
+            error.classList.add('error');
+            form.insertBefore(error, node);
+        }
+    }
+
+    function removeErrorToElement(node) {
+        previousNode = node.previousSibling;
+        if (previousNode !== undefined && previousNode.tagName === 'DIV' && previousNode.classList.contains('error')){
+            form.removeChild(previousNode);
+        }
+    }
+
+    function getAge(date) {
+        var today = new Date();
+        var userDate = new Date(date);
+        var age = today.getFullYear() - userDate.getFullYear();
+        var m = today.getMonth() - userDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < userDate.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
+    function generateNumber(min, max) {
+        return Math.floor((Math.random() * max - min)) + min;
+    }
+
 })();
--- Tema5/formulario/formulario.js
+++ Tema5/formulario/formulario.js
@@ -9,7 +9,7 @@
 		var email = document.getElementById("email").value;
 		 
 		let salida = "";
-	 	if(nombre.trim().length < 3 || nombre.trim().length > 10 || apellido.trim().length < 3 && apellido.trim().length > 10) {
+	 	if(nombre.trim().length < 3 || nombre.trim().length > 10 || apellido.trim().length < 3 || apellido.trim().length > 10) {
 			salida += '- Nombre/Apellidos requerido entre 3-10 caracteres\n\n';
 	 	}
 
@@ -25,7 +25,7 @@
 
 		function validar_email( email ) {
 			var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-			return regex.test(email) ? true : false;
+			return regex.test(email);
 		}
 	}
 })();	
