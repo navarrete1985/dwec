3d80e50f11146c8532414e9869a57a379fb12b51
Nacho <nacho.pena.navarrete@gmail.com>
2018-12-10 12:50:35 +0100

    uso libreria jquery.ui

---
+++ Objetos/ejemplosJquery/faq/script.js
@@ -0,0 +1,22 @@
+$( function() {
+    $( "#accordion" ).accordion({
+        collapsible: true,
+        active: false
+    });
+});
+
+$( function() {
+    $( "#tab" ).tabs({
+      event: 'mouseover'
+    });
+});
+
+$( function() {
+    var tabs = $( "#tabs2" ).tabs();
+    tabs.find( ".ui-tabs-nav" ).sortable({
+      axis: "x",
+      stop: function() {
+        tabs.tabs( "refresh" );
+      }
+    });
+  } );
