b4f55707c2b58a5980380acc1696f197de989f0b
Luis Ignacio <nacho.pena.navarrete@gmail.com>
2019-01-14 18:29:48 +0100

    Ejercicio fetch

--- Ajax/fetch-promises/script.js
+++ Ajax/fetch-promises/script.js
@@ -1,29 +1,12 @@
 (function() {
     let key = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJuYWNob19wZW5hQGhvdG1haWwuY29tIiwianRpIjoiZjBlNjdhNmEtOWQzMi00NTkzLWFmZTktYTBiMjVhYTY5YzkwIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE1NDY5MzY5NzYsInVzZXJJZCI6ImYwZTY3YTZhLTlkMzItNDU5My1hZmU5LWEwYjI1YWE2OWM5MCIsInJvbGUiOiIifQ.EgyVfXO-0_iPv9e5nRNvFQKC2FkltEe1vFz9rdzsV8I";
     var url = `https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/18087/?api_key=${key}`;
-    var solicitud = new XMLHttpRequest();
-    solicitud.open("GET", url);
-    solicitud.onload = function() {
-        if (solicitud.status == 200) {
-            let obj = JSON.parse(solicitud.responseText);
-            obtenerDatos(obj.datos);
-        }
-    };
-    solicitud.send(null);
-
-    function obtenerDatos(url) {
-        let request = new XMLHttpRequest();
-        request.open('GET', url);
-        request.onload = () => {
-            if (request.status == 200) {
-                let data = JSON.parse(request.responseText);
-                pintarDatos(data);
-            }
-        } 
-        request.send(null);
-    }
-
-    function pintarDatos(data) {
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(response => fetch(response.datos))
+    .then(response => response.json())
+    .then(data => {
         data.forEach(element => {
             element.prediccion.dia.forEach((item) => {
                 createRow(item.fecha, item.probPrecipitacion[0].value, 
@@ -31,7 +14,7 @@
                     item.sensTermica)
             })
         });
-    }
+    })
 
     function createRow(dia, probLluvia, humRel, viento, temp, sensTer) {
         $('table.table tbody').append(
@@ -45,4 +28,5 @@
             </tr>`
         );
     }
-}());
+    
+})();
